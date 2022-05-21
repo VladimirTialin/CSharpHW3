437dde1db7d3ea41964071b1b1f60bf036d2da82
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-21 11:32:02 +0300

    задача 18 версия 3 с функцией

---
+++ Example_v.3/Program.cs
@@ -0,0 +1,35 @@
+﻿try
+{
+    Console.Write("Введите число: ");
+    int NumberOfPalindrom = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Наше число является палиндромом? ");
+    Palindrom(NumberOfPalindrom);
+}
+catch
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Ошибочка вышла. Повтори ввод!");
+    Console.ResetColor();
+}
+// Функция палиндром, наверное самая удачная у меня))
+ void Palindrom(int number)
+{
+    int result = 0;
+    int temp = number;
+   while (number > 0)
+    {
+        result = result * 10 + number % 10;
+        number /= 10;
+    }
+    if (temp == result)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"{temp} - > да");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"{temp} - > нет");
+    }
+    Console.ResetColor();
+}
